20e9b1d4ffda7b8b679f3234164f6fa52645af1d
Alina Leonova <alinaa.leonova@gmail.com>
2020-09-24 14:15:25 +0300

    решения задачи 1-1

---
+++ .js/task-1.js
@@ -0,0 +1,8 @@
+const name = "Генератор захисного поля"
+let price = 1000
+const firstMessage = `Обрано ${name}, ціна за штуку ${price} кредитів`
+price = 2000
+const secondMessage = `Обрано ${name}, ціна за штуку ${price} кредитів`
+
+console.log(firstMessage)
+console.log(secondMessage)
