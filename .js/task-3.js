f3c6ad37b04007b0bf786357bcdd30736c3e61e9
Alina Leonova <alinaa.leonova@gmail.com>
2020-09-27 12:54:29 +0300

    задача 3

---
+++ .js/task-3.js
@@ -0,0 +1,23 @@
+const userPassword = null;
+
+const ADMIN_PASSWORD = 'jqueryismyjam';
+
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
+const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
+let message;
+
+if (userPassword === ADMIN_PASSWORD) { message = ACCESS_IS_ALLOWED } else {
+    console.log(ACCESS_DENIED)
+};
+if (userPassword === null) { message = CANCELED_BY_USER };
+
+console.log(message);
+
+// if (userPassword === ADMIN_PASSWORD) { message = ACCESS_IS_ALLOWED } else {
+//     //console.log(ACCESS_DENIED)
+// };
+// if (userPassword === null) { message = CANCELED_BY_USER };
+// if (userPassword === 'bla') { message = ACCESS_DENIED };
+
+// console.log(message);
