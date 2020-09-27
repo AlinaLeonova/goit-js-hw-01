09813f074a2d8c3b3a7ba6aa2163828ff9d1d287
Alina Leonova <alinaa.leonova@gmail.com>
2020-09-27 12:54:07 +0300

    задача 2

---
+++ .js/task-2.js
@@ -0,0 +1,7 @@
+const invoice = 100;
+const stock = 100;
+
+// Write code under this line
+let message = invoice > stock ? "На складе недостаточно товаров" : "Заказ оформлен, с вами свяжется менеджер";
+
+console.log(message);
